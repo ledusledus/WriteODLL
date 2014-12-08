0360f2fd7d9333fc8a788dc0fd7cfc55796b82e2
ledusledus <peteris.ledins@gmail.com>
2014-12-07 21:33:09 -0800

    missed files

--- writeodll/stdafx.cpp
+++ writeodll/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// WriteOcad.pch will be the pre-compiled header
+// WriteODLL.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
